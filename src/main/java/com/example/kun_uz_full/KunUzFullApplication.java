c8bde68379e105e8e1546f4dd9528a7ddd802954
Orazgali <77142550+Urazgali@users.noreply.github.com>
2023-08-05 15:30:39 +0500

    first commit

---
+++ src/main/java/com/example/kun_uz_full/KunUzFullApplication.java
@@ -0,0 +1,13 @@
+package com.example.kun_uz_full;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KunUzFullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KunUzFullApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kun_uz_full/KunUzFullApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kun_uz_full;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KunUzFullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
